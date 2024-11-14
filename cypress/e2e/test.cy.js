6f400380dec053d613b4e97116890b3df6cbedc1
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-11-14 10:00:51 +0100

    Added code for cypress test
    Co-authored-by: annaisakson <annaisakson@users.noreply.github.com>
    Co-authored-by: Chargermaster <Chargermaster@users.noreply.github.com>
    Co-authored-by: Regni <Regni@users.noreply.github.com>
    Co-authored-by: Johan <jhn322@users.noreply.github.com>

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,14 @@
+const testUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://main.d2iuui4uss8fdt.amplifyapp.com"
+    : "http://localhost:5173";
+
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit(testUrl);
+
+    cy.get('[data-testid="MarketPro-title"]')
+      .should("exist")
+      .should("have.text", "MarketPro");
+  });
+});
