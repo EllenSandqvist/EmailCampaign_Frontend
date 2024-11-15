3b8b25c4f4f844f515c158f2f9a90ebf9a68cdcd
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-11-15 17:02:44 +0100

    Added en other test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,6 +3,18 @@
 //     ? "https://main.d2iuui4uss8fdt.amplifyapp.com"
 //     : "http://localhost:5173";
 
+describe("Register page tests", () => {
+  it("Verifies register page elements", () => {
+    cy.visit("https://main.d2iuui4uss8fdt.amplifyapp.com/register", {
+      failOnStatusCode: false,
+    });
+
+    cy.get('[id="email"]')
+      .should("exist")
+      .and("have.attr", "placeholder", "Enter your email");
+  });
+});
+
 describe("template spec", () => {
   it("passes", () => {
     cy.visit("https://main.d2iuui4uss8fdt.amplifyapp.com");
