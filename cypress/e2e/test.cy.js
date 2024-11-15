e93e886293edfeee7b0e57f6d0f0d27b1b9d474b
Ellen Sandqvist <ell1sandqvist@gmail.com>
2024-11-15 16:17:26 +0100

    changed test.cy.js to only include deployed URL

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,11 +1,11 @@
-const testUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://main.d2iuui4uss8fdt.amplifyapp.com"
-    : "http://localhost:5173";
+// const testUrl =
+//   process.env.NODE_ENV === "production"
+//     ? "https://main.d2iuui4uss8fdt.amplifyapp.com"
+//     : "http://localhost:5173";
 
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit(testUrl);
+    cy.visit("https://main.d2iuui4uss8fdt.amplifyapp.com");
 
     cy.get('[data-testid="MarketPro-title"]')
       .should("exist")
